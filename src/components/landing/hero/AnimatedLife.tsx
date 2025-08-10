449a5971bd67ecef5413d512f00ca34186a963fe
CinquinAndy <cinquin.andy@gmail.com>
2025-08-10 22:08:17 +0200

    feat(ui): enhance animated components with theme support
    - Integrate theme context for color customization
    - Update Cycling and Runner components to accept color props
    - Adjust SVG fill and stroke attributes based on the current theme
    - Simplify layout structure for better responsiveness

--- src/components/landing/hero/AnimatedLife.tsx
+++ src/components/landing/hero/AnimatedLife.tsx
@@ -1,13 +1,20 @@
+'use client'
+
+import { useThemeStore } from '@/hooks/useTheme'
+
 export function AnimatedLife() {
+	const { theme } = useThemeStore()
 	return (
-		<div className="absolute bottom-0 left-0 z-20 flex h-[25vh] w-screen gap-20">
-			<Cycling />
-			<Runner />
+		<div className="absolute bottom-0 left-0 z-20 flex h-[25vh] w-screen">
+			<div className="relative h-full w-full">
+				<Cycling color={theme === 'dark' ? 'white' : 'black'} />
+				<Runner color={theme === 'dark' ? 'white' : 'black'} />
+			</div>
 		</div>
 	)
 }
 
-function Cycling() {
+function Cycling({ color }: { color: string }) {
 	return (
 		<svg
 			width="800"
@@ -19,23 +26,23 @@ function Cycling() {
 		>
 			<path
 				d="M644.677 410.63C730.333 410.63 800 480.325 800 565.981C800 651.609 730.333 721.277 644.677 721.277C559.034 721.277 489.367 651.609 489.367 565.981C489.367 480.325 559.034 410.63 644.677 410.63ZM644.677 678.727C706.867 678.727 757.436 628.142 757.436 565.98C757.436 503.777 706.866 453.206 644.677 453.206C582.502 453.206 531.917 503.778 531.917 565.98C531.919 628.142 582.502 678.727 644.677 678.727Z"
-				fill="black"
+				fill={color}
 			/>
 			<path
 				d="M155.323 410.63C240.966 410.63 310.633 480.325 310.633 565.981C310.633 651.609 240.966 721.277 155.323 721.277C69.6672 721.277 1.14441e-05 651.609 1.14441e-05 565.981C1.14441e-05 480.325 69.6672 410.63 155.323 410.63ZM155.323 678.727C217.498 678.727 268.083 628.142 268.083 565.98C268.083 503.777 217.498 453.206 155.323 453.206C93.1344 453.206 42.55 503.778 42.55 565.98C42.55 628.142 93.1344 678.727 155.323 678.727Z"
-				fill="black"
+				fill={color}
 			/>
 			<path
 				d="M393.791 136.427L612.578 170.467C652.644 179.588 650.636 196.35 645.6 222.748L621.278 310.983L706.391 311.703L706.377 311.906C717.248 313.264 725.677 322.52 725.677 333.744C725.677 345.933 715.795 355.786 703.634 355.786L587.698 367.444C565.833 368.394 558.517 348.416 558.967 331.925L563.772 276.073L482.528 253.217C482.528 253.217 521.969 300.028 525.241 304.114C541.581 324.541 547.5 361.986 521.155 381.748L407.106 464.555L400.619 563.852C400.619 580.288 387.291 593.616 370.855 593.616C354.433 593.616 341.105 580.288 341.105 563.852C341.105 563.2 314.909 245.767 314.909 245.767C307.334 179.586 345.405 128.813 393.791 136.427ZM453.889 356.763C459.562 352.514 458.164 346.841 453.889 341.167C449.655 335.48 415.614 307.114 415.614 307.114L408.503 396.475L453.889 356.763Z"
-				fill="black"
+				fill={color}
 			/>
 			<path
 				d="M761.02 154.247H702.863V223.234H694.094V154.247H664.859C664.859 154.247 634.172 138.747 620.681 123.261C616.908 118.917 614.681 108.656 627.359 111.303C627.359 111.303 620.355 94.9344 618.089 88.6094C615.822 82.2578 617.628 76.4485 630.277 79.6125C636.764 81.2407 670.411 87.6735 717.983 98.0969C761.347 107.613 772.719 141.57 772.719 147.202C772.719 152.836 770.616 154.247 761.02 154.247Z"
-				fill="black"
+				fill={color}
 			/>
-			<path d="M767.183 180.998L754.519 192.602L717.494 182.058V164.114H767.183V180.998Z" fill="black" />
+			<path d="M767.183 180.998L754.519 192.602L717.494 182.058V164.114H767.183V180.998Z" fill={color} />
 
-			<path d="M18 563.5H290.5" stroke="black" strokeWidth="25">
+			<path d="M18 563.5H290.5" stroke={color} strokeWidth="25">
 				<animateTransform
 					attributeName="transform"
 					attributeType="XML"
@@ -46,7 +53,7 @@ function Cycling() {
 					repeatCount="indefinite"
 				/>
 			</path>
-			<path d="M508 563.5H780.5" stroke="black" strokeWidth="25">
+			<path d="M508 563.5H780.5" stroke={color} strokeWidth="25">
 				<animateTransform
 					attributeName="transform"
 					attributeType="XML"
@@ -61,7 +68,7 @@ function Cycling() {
 	)
 }
 
-function Runner() {
+function Runner({ color }: { color: string }) {
 	return (
 		<svg
 			width="800"
@@ -72,13 +79,7 @@ function Runner() {
 			className="h-10 w-10"
 		>
 			{/* right leg */}
-			<path
-				d="M583 750L530 559L388.5 419"
-				stroke="#FF0202"
-				strokeWidth="80"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			>
+			<path d="M583 750L530 559L388.5 419" stroke={color} strokeWidth="80" strokeLinecap="round" strokeLinejoin="round">
 				<animate
 					attributeName="d"
 					values="M583 750L530 559L388.5 419; M135 625L315 625L388.5 419; M583 750L530 559L388.5 419"
@@ -90,15 +91,9 @@ function Runner() {
 				/>
 			</path>
 			{/* body */}
-			<path d="M388 419L430 199" stroke="#0000FF" strokeWidth="80" strokeLinecap="round" />
+			<path d="M388 419L430 199" stroke={color} strokeWidth="80" strokeLinecap="round" />
 			{/* left leg */}
-			<path
-				d="M135 625L315 625L388.5 419"
-				stroke="#FF0202"
-				strokeWidth="80"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			>
+			<path d="M135 625L315 625L388.5 419" stroke={color} strokeWidth="80" strokeLinecap="round" strokeLinejoin="round">
 				<animate
 					attributeName="d"
 					values="M135 625L315 625L388.5 419; M583 750L530 559L388.5 419; M135 625L315 625L388.5 419"
@@ -112,7 +107,7 @@ function Runner() {
 			{/* arm */}
 			<path
 				d="M430 201L268 259L207.5 372.5"
-				stroke="#FFFF00"
+				stroke={color}
 				strokeWidth="80"
 				strokeLinecap="round"
 				strokeLinejoin="round"
@@ -128,13 +123,7 @@ function Runner() {
 				/>
 			</path>
 			{/* arm */}
-			<path
-				d="M430 201L532 366L660.5 348"
-				stroke="#82821A"
-				strokeWidth="80"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			>
+			<path d="M430 201L532 366L660.5 348" stroke={color} strokeWidth="80" strokeLinecap="round" strokeLinejoin="round">
 				<animate
 					attributeName="d"
 					values="M430 201L532 366L660.5 348; M430 201L280 257L208 369.5; M430 201L532 366L660.5 348"
@@ -148,7 +137,7 @@ function Runner() {
 			{/* head (no anim)*/})
 			<path
 				d="M449.063 137.188C467.188 137.188 482.5 130.937 495.313 118.125C508.125 105.312 514.375 90.3126 514.375 73.1251C514.375 55.0001 508.125 39.6874 495.313 26.8749C482.5 14.0624 467.188 7.8125 449.063 7.8125C431.563 7.8125 416.563 14.0624 403.75 26.8749C390.938 39.6874 384.688 55.0001 384.688 73.1251C384.688 90.6251 390.938 105.625 403.75 118.125C416.875 130.937 431.875 137.188 449.063 137.188Z"
-				fill="black"
+				fill={color}
 			></path>
 		</svg>
 	)
