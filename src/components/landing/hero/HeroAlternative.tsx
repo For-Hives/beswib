0eb3ed76c4071d27afec83494b5ed0d56364b9b4
CinquinAndy <cinquin.andy@gmail.com>
2025-08-12 00:39:25 +0200

    feat(ui): update landing hero background image
    - Add new WebP format background image for better performance
    - Replace PNG source with WebP in HeroAlternative component

--- src/components/landing/hero/HeroAlternative.tsx
+++ src/components/landing/hero/HeroAlternative.tsx
@@ -21,7 +21,7 @@ export default async function HeroAlternative({ localeParams }: { localeParams: 
 					fill
 					// size= 100vw in tablet and 80vw on phone
 					sizes="100vw"
-					src={'/landing/background_v2.png'}
+					src={'/landing/background_v2.webp'}
 				/>
 			</div>
 			<AnimatedLife />
