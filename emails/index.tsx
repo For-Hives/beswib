e8185dc97e76a1cba3452478b5c177b197f628bc
CinquinAndy <cinquin.andy@gmail.com>
2025-08-22 02:27:15 +0200

    fix(emails): remove email preview file from project scope

--- emails/index.tsx
+++
@@ -1,7 +0,0 @@
-// React Email preview file for development
-// Run: npx react-email preview or pnpm react-email preview
-
-import { BeswibEmailVerification } from '../src/components/emails/BeswibEmailVerification'
-import { BeswibWelcomeEmail } from '../src/components/emails/BeswibWelcomeEmail'
-
-export { BeswibEmailVerification, BeswibWelcomeEmail }
