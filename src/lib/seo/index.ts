eb6b1a1265252f2df1debd0b8cb1c0e487ef0f20
CinquinAndy <cinquin.andy@gmail.com>
2025-08-22 21:30:41 +0200

    fix(seo): Remove unused export of types from index.ts

--- src/lib/seo/index.ts
+++ src/lib/seo/index.ts
@@ -1,7 +1,6 @@
 // Export all SEO utilities and components
 export * from './constants/seo-translations'
 export { default as seoLocales } from './constants/seo-locales.json'
-export * from './types'
 export * from './utils/seo-generators'
 export * from './metadata-generators'
 
